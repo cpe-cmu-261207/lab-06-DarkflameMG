e3278edd90dbba22cd205e18bd71520bd8f070ab
Bank <deknan70@gmail.com>
2021-08-26 14:36:29 +0700

    component add

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Current from './components/Current';
+import About from './components/About';
 
 function App() {
   return (
     <Router>
       <Navbar />
 
-      {/* template for /current */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>Current price</p>
-        <p className='text-2xl'>Loading ...</p>
-        <p className='text-2xl'>{(999999999).toLocaleString()} THB</p>
-        <p> (Last updated) </p>
-      </div>
-
-      <br />
-
-      {/* template for /history/select */}
-      <div className='text-center space-y-3 space-x-3'>
-        <p className='text-2xl font-semibold'>Select historical range</p>
-        <span>From date</span>
-        <input type='date' onChange={e => console.log(e.target.value)}></input>
-        <span>To date</span>
-        <input type='date' onChange={e => console.log(e.target.value)}></input>
-        <br />
-        <button>Get data</button>
-      </div>
-
-      <br />
-
-      {/* template for /history/result */}
+      {/* template for /history/result
       <div className='text-center space-y-3'>
         <p className='text-2xl font-semibold'>Historical price</p>
         <p className='text-2xl'>Loading ...</p>
@@ -41,15 +20,29 @@ function App() {
           <li className='text-xl'>2021-01-02 - {(2000000).toLocaleString()} THB</li>
           <li className='text-xl'>2021-01-03 - {(3000000).toLocaleString()} THB</li>
         </ul>
-      </div>
+      </div> */}
 
-      <br />
 
-      {/* template for about me */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>About me</p>
-        <p className='text-xl'>Chayanin Suatap 610631100</p>
-      </div>
+      <Switch>
+        <Route path='/current'>
+          <Current/>
+        </Route>
+        <Route path='/history/select'>
+          {/* template for /history/select */}
+          <div className='text-center space-y-3 space-x-3'>
+            <p className='text-2xl font-semibold'>Select historical range</p>
+            <span>From date</span>
+            <input type='date' onChange={e => console.log(e.target.value)}></input>
+            <span>To date</span>
+            <input type='date' onChange={e => console.log(e.target.value)}></input>
+            <br />
+            <button>Get data</button>
+          </div>
+        </Route>
+        <Route path='/about'>
+          <About/>
+        </Route>
+      </Switch>
 
     </Router>
   );
---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>Khanitin Saenkan 630610718</p>
+        </div>
+    )
+}
+
+export default About
